c0b8326f41a0eb9aedda2e07f5e24b5c70bf07a4
Roberto Menegassi <81974099+RMenegassi@users.noreply.github.com>
2023-02-19 00:47:06 -0300

    cleaning RN template

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text>Results Tracker</Text>
+    </View>
+  );
+}
+
+export default App;
