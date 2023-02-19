63fa26df6eb7a9329fe425cecf01db86eab2584c
Roberto Menegassi <81974099+RMenegassi@users.noreply.github.com>
2023-02-19 16:33:06 -0300

    add functions to change days

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,40 +1,59 @@
-import {View, Text, TouchableOpacity, Button} from 'react-native';
 import React, {useState} from 'react';
 
+import Header from '../../components/Header';
+import Report from '../../components/Report';
+import GraphPie from '../../components/GraphPie';
+
+import {Container, TextTitle, Button, TextButtons} from './styles';
+
 const Main = () => {
+  const [day, setDay] = useState(new Date());
   const [vitorias, setVitorias] = useState(0);
   const [derrotas, setDerrotas] = useState(0);
-  const data = '19/02/2023';
+
+  const data = [
+    {day: '19/02/2023', vitorias: 10, derrotas: 1},
+    {day: '20/02/2023', vitorias: 1, derrotas: 5},
+    {day: '18/02/2023', vitorias: 3, derrotas: 2},
+    {day: '15/02/2023', vitorias: 6, derrotas: 2},
+    {day: '25/02/2023', vitorias: 5, derrotas: 0},
+  ];
+
+  const subDay = () => {
+    const newDay = new Date(day);
+    newDay.setDate(newDay.getDate() - 1);
+    setDay(newDay);
+  };
+
+  const addDay = () => {
+    const newDay = new Date(day);
+    newDay.setDate(newDay.getDate() + 1);
+    setDay(newDay);
+  };
 
   return (
-    <View style={{alignItems: 'center'}}>
-      <View style={{flexDirection: 'row'}}>
-        <TouchableOpacity>
-          <Text>Back</Text>
-        </TouchableOpacity>
-        <Text>{data}</Text>
-        <TouchableOpacity>
-          <Text>Next</Text>
-        </TouchableOpacity>
-      </View>
-      <Text>Partida {vitorias + derrotas + 1}</Text>
-      <Button title="Vitorias" onPress={() => setVitorias(vitorias + 1)} />
-      <Button title="Derrotas" onPress={() => setDerrotas(derrotas + 1)} />
-      <View>
-        <Text>Partidas {vitorias + derrotas}</Text>
-        <View style={{flexDirection: 'row'}}>
-          <Text> Vitórias :{vitorias}</Text>
-          <Button title="-" onPress={() => setVitorias(vitorias - 1)} />
-        </View>
-        <View style={{flexDirection: 'row'}}>
-          <Text> Derrotas :{derrotas}</Text>
-          <TouchableOpacity>
-            <Button title="-" onPress={() => setDerrotas(derrotas - 1)} />
-          </TouchableOpacity>
-        </View>
-      </View>
-      <Text>Gráfico</Text>
-    </View>
+    <Container>
+      <Header
+        day={day.toLocaleDateString('pt-BR')}
+        subDay={subDay}
+        addDay={addDay}
+      />
+
+      <TextTitle>Partida {vitorias + derrotas + 1}</TextTitle>
+      <Button title="vitoria" onPress={() => setVitorias(vitorias + 1)}>
+        <TextButtons>Vitórias</TextButtons>
+      </Button>
+      <Button title="derrota" onPress={() => setDerrotas(derrotas + 1)}>
+        <TextButtons>Derrotas</TextButtons>
+      </Button>
+
+      <Report
+        results={{victory: vitorias, loss: derrotas}}
+        setVitorias={setVitorias}
+        setDerrotas={setDerrotas}
+      />
+      <GraphPie results={{victory: vitorias, loss: derrotas}} />
+    </Container>
   );
 };
 
