8959c8e7c2dd961ab215ef3fe86598be0439903c
Roberto Menegassi <81974099+RMenegassi@users.noreply.github.com>
2023-02-19 23:01:01 -0300

    setup save and update logic

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -10,11 +10,12 @@ import useEntries from '../../hooks/useResults';
 
 const Main = () => {
   const [day, setDay] = useState(new Date());
-  const [vitorias, setVitorias] = useState(0);
-  const [derrotas, setDerrotas] = useState(0);
-  const [data] = useEntries(day.toLocaleDateString('pt-BR'));
-
-  console.log(data);
+  const [clicked, setClicked] = useState(false);
+  const [data, addEntry, updateEntry] = useEntries(
+    day.toLocaleDateString('pt-BR'),
+    0,
+    clicked,
+  );
 
   const subDay = () => {
     const newDay = new Date(day);
@@ -26,6 +27,31 @@ const Main = () => {
     const newDay = new Date(day);
     newDay.setDate(newDay.getDate() + 1);
     setDay(newDay);
+  };
+
+  const updateVictory = valor => {
+    updateEntry({
+      ...data[0],
+      victory: data[0].victory + valor,
+    });
+    setClicked(!clicked);
+  };
+
+  const updateLoss = valor => {
+    updateEntry({
+      ...data[0],
+      loss: data[0].loss + valor,
+    });
+    setClicked(!clicked);
+  };
+
+  const createNewSave = type => {
+    addEntry({
+      day: day,
+      victory: type === 'victory' ? 1 : 0,
+      loss: type === 'loss' ? 1 : 0,
+    });
+    setClicked(!clicked);
   };
 
   return (
@@ -40,14 +66,28 @@ const Main = () => {
         Partida {data.length > 0 ? data[0].victory + data[0].loss + 1 : '1'}
       </TextTitle>
 
-      <Button title="vitoria" onPress={() => setVitorias(vitorias + 1)}>
+      <Button
+        title="vitoria"
+        onPress={
+          data.length > 0
+            ? () => updateVictory(1)
+            : () => createNewSave('victory')
+        }>
         <TextButtons>Vitórias</TextButtons>
       </Button>
-      <Button title="derrota" onPress={() => setDerrotas(derrotas + 1)}>
+      <Button
+        title="derrota"
+        onPress={
+          data.length > 0 ? () => updateLoss(1) : () => createNewSave('loss')
+        }>
         <TextButtons>Derrotas</TextButtons>
       </Button>
 
-      <Report data={data} setVitorias={setVitorias} setDerrotas={setDerrotas} />
+      <Report
+        data={data}
+        updateVictory={updateVictory}
+        updateLoss={updateLoss}
+      />
       <GraphPie data={data} />
     </Container>
   );
