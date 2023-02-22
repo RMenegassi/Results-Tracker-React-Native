8520d46c4043fb21289a37f2e58b8dc3898efdec
Roberto Menegassi <81974099+RMenegassi@users.noreply.github.com>
2023-02-21 21:02:38 -0300

    update Statistics component to use variables

--- src/Pages/Stats/index.js
+++ src/Pages/Stats/index.js
@@ -4,15 +4,18 @@ import ModalTime from '../../components/ModalTime';
 import Statistics from '../../components/Statistics';
 import GraphStats from '../../components/GraphStats';
 
+import useStats from '../../hooks/useStats';
+
 import {Container} from './styles';
 
 const Stats = () => {
   const [days, setDays] = useState(0);
+  const [entries] = useStats(days);
 
   return (
     <Container>
       <ModalTime days={days} setDays={setDays} />
-      <Statistics />
+      <Statistics data={entries} />
       <GraphStats />
     </Container>
   );
--- src/components/Statistics/index.js
+++ src/components/Statistics/index.js
@@ -2,28 +2,39 @@ import React from 'react';
 
 import {Container, Title, ContainerContent, Text} from './styles';
 
-const Statistics = () => {
+const Statistics = ({data}) => {
+  const sumMatches = data[1].victory + data[1].loss;
+
+  const percentTotal = (
+    (data[1].victory * 100) /
+    (data[1].victory + data[1].loss)
+  ).toFixed(0);
+
+  const percentVictoryDay = (
+    (data[2].victory * 100) /
+    (data[2].victory + data[2].loss)
+  ).toFixed(0);
+
   return (
     <Container>
       <Title>Durante esse período</Title>
       <ContainerContent>
-        <Text>Você jogou: 150 partidas {'\n'}</Text>
-        <Text>Venceu: 90 partidas</Text>
-        <Text>Perdeu: 60 partidas</Text>
-        <Text>Aproveitamento de 60% {'\n'}</Text>
         <Text>
-          Em 19/02/2023 você ganhou 15 partidas com 65% de aproveitamento{'\n'}
+          Você jogou: {sumMatches} partidas {'\n'}
+        </Text>
+        <Text>Venceu: {data[1].victory} partidas</Text>
+        <Text>Perdeu: {data[1].loss} partidas</Text>
+        <Text>
+          Aproveitamento de {percentTotal}% {'\n'}
         </Text>
         <Text>
-          Em 18/02/2023 você teve 18 derrotas com 65% de aproveitamento{'\n'}
-        </Text>
-        <Text>
-          O mês com maior número de vitórias foi: Abril de 2023 com 85 vitorias
+          Em {data[2].day.toDate().toLocaleDateString('pt-BR')} você ganhou{' '}
+          {data[2].victory} partidas com {percentVictoryDay}% de aproveitamento
           {'\n'}
         </Text>
         <Text>
-          O mês com maior número de vitórias foi: Abril de 2023 com 85 vitorias
-          {'\n'}
+          Em {data[3].day.toDate().toLocaleDateString('pt-BR')} você teve{' '}
+          {data[3].loss} derrotas {'\n'}
         </Text>
       </ContainerContent>
     </Container>
